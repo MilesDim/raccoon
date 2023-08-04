0a273580b0a7ae3ac630a22104512e81455d6674
MilesDim <137657161+MilesDim@users.noreply.github.com>
2023-08-04 20:51:19 +0300

    link to my itch
    Adding a link to my itch

---
+++ Assets/Scripts/Link to my itch.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linktomyitch : MonoBehaviour
+{
+   public void Linkto()
+   {
+    Application.OpenURL("https://milesche.itch.io/");
+   }
+}
