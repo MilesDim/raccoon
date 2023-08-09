62a2c5c88e04f0349ab1899561870bc349d40689
MilesDim <137657161+MilesDim@users.noreply.github.com>
2023-08-09 19:33:42 +0300

    Adding skinning

--- Assets/Scripts/LanguageManager.cs
+++ Assets/Scripts/LanguageManager.cs
@@ -13,11 +13,10 @@ public class LanguageManager : MonoBehaviour
 
     private int currentLanguageIndex = 0;
     public TMP_FontAsset font_en;
-    
-    List<TMP_Text> textObjects;
     private int score = 0;
+    private const string LanguageSelectionKey = "LanguageSelection";
 
-    private void Awake()
+     private void Awake()
     {
         languageFonts = new Dictionary<string, TMP_FontAsset>();
         languageFonts.Add("en", font_en);
@@ -25,38 +24,38 @@ public class LanguageManager : MonoBehaviour
         // Загрузите выбранный язык и установите его
         string selectedLanguage = LoadSelectedLanguage();
         ChangeLanguage(true); // Установите выбранный язык вперед
+
+        // Примените шрифт для текущего языка после загрузки
+        ApplyFontForCurrentLanguage(selectedLanguage);
     }
 
-    public void addTextObject(TMP_Text textObject){
-        textObjects.Add(textObject);
-    }
-
-   public void ChangeLanguage(bool forward)
-   {
-    int languageCount = LocalizationSettings.AvailableLocales.Locales.Count;
-    
-    if (forward)
+    public void ChangeLanguage(bool forward)
     {
-        currentLanguageIndex = (currentLanguageIndex + 1) % languageCount;
-    }
-    else
-    {
-        currentLanguageIndex = (currentLanguageIndex - 1 + languageCount) % languageCount;
+        int languageCount = LocalizationSettings.AvailableLocales.Locales.Count;
+
+        if (forward)
+        {
+            currentLanguageIndex = (currentLanguageIndex + 1) % languageCount;
+        }
+        else
+        {
+            currentLanguageIndex = (currentLanguageIndex - 1 + languageCount) % languageCount;
+        }
+
+        // Получите текущий выбранный язык и его код
+        var selectedLanguage = LocalizationSettings.AvailableLocales.Locales[currentLanguageIndex];
+        var selectedLanguageCode = selectedLanguage.Identifier.Code;
+
+        // Сохраните выбранный язык
+        SaveSelectedLanguage(selectedLanguageCode);
+
+        // Примените шрифт для текущего языка
+        ApplyFontForCurrentLanguage(selectedLanguageCode);
     }
 
-    // Получите текущий выбранный язык и его код
-    var selectedLanguage = LocalizationSettings.AvailableLocales.Locales[currentLanguageIndex];
-    var selectedLanguageCode = selectedLanguage.Identifier.Code;
-
-    // Сохраните выбранный язык
-    SaveSelectedLanguage(selectedLanguageCode);
-
-    // Примените шрифт для текущего языка
-    ApplyFontForCurrentLanguage(selectedLanguageCode);
-    }
     private void SaveSelectedLanguage(string languageCode)
     {
-    PlayerPrefs.SetString("SelectedLanguage", languageCode);
+        PlayerPrefs.SetString(LanguageSelectionKey, languageCode);
     }
 
     private void ApplyFontForCurrentLanguage(string languageCode)
@@ -64,36 +63,14 @@ public class LanguageManager : MonoBehaviour
         // Применяем шрифт для текущего языка, если он доступен
         if (languageFonts.TryGetValue(languageCode, out TMP_FontAsset font))
         {
-            TMP_Text[] textElements = Resources.FindObjectsOfTypeAll(typeof(TMP_Text)) as TMP_Text[]; 
+            TMP_Text[] textElements = FindObjectsOfType<TMP_Text>();
             foreach (TMP_Text textElement in textElements)
             {
-                textElement.font = font; // Используем свойство font для установки шрифта
-            } 
+                textElement.font = font;
+            }
         }
     }
 
-
-  private void ApplyFontForCurrentLanguage2(string languageCode)
-    {
-        // Применяем шрифт для текущего языка, если он доступен
-        if (languageFonts.TryGetValue(languageCode, out TMP_FontAsset font))
-        {
-            // Обновляем шрифт для всех объектов TMP_Text
-            TMP_Text[] tmpTextElements = FindObjectsOfType<TMP_Text>();
-            foreach (TMP_Text textElement in tmpTextElements)
-            {
-                textElement.font = font; // Используем свойство font для установки шрифта
-            }
-
-            // Обновляем шрифт для всех объектов TMPro.TextMeshProUGUI
-            TextMeshProUGUI[] textMeshProElements = FindObjectsOfType<TextMeshProUGUI>();
-            foreach (TextMeshProUGUI textMeshProElement in textMeshProElements)
-            {
-                textMeshProElement.font = font; // Используем свойство font для установки шрифта
-            }
-
-        }
-    }
     public void ChangeScore(bool forward)
     {
         if (forward)
@@ -104,12 +81,11 @@ public class LanguageManager : MonoBehaviour
         {
             score--;
         }
-
     }
+
     private string LoadSelectedLanguage()
     {
-    return PlayerPrefs.GetString("SelectedLanguage", "en"); // "en" - язык по умолчанию
+        string loadedLanguage = PlayerPrefs.GetString(LanguageSelectionKey, "en");
+        return loadedLanguage;
     }
-
-
 }
